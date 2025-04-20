822a7a74cda89e03a660e8d9c11d3d35d774b131
Andrea Lavino <andrealavino1410.2003@gmail.com>
2025-04-20 21:28:21 +0200

    feat: implemented image subsampling and improved image to ascii conversion

--- asciifier.py
+++ asciifier.py
@@ -1,49 +1,85 @@
 import skimage as ski
-import matplotlib.pyplot as plt
 import numpy as np
 import numpy.typing as npt
 
-def get_level_char(intensity, max_value = 1):
+def quantizeGrayscale(image):
+    scaling_factor = 1/256
+
+    return np.clip(image / scaling_factor, a_min=0, a_max=255)
+
+def is_rgb(image):
+    return len(image.shape) > 2
+
+def get_level_char(intensity, max_value = 255):
     levels = """$@B%8&WM#*oahkbdpqwmZO0QLCJUYXzcvunxrjft/\\|()1{}[]?-_+~<>i!lI;:,"^`'. """
     levels = levels[::-1]
 
-    if not type(intensity) is int:
-        intensity = 0.2125*intensity[0] + 0.7154*intensity[1] + 0.0721*intensity[2]
+    if len(intensity) == 1:
+        intensity = intensity[0]
+    else:
+        intensity = 0.2125 * intensity[0] + 0.7154 * intensity[1] + 0.0721 * intensity[2]
 
     level_index = round((intensity * len(levels)) / max_value) - 1
 
     return  levels[level_index]
 
-def convertGS(image: npt.NDArray, output_file='output.txt'):
-    """
-        image: is the grayscale image to convert
-    """
+def subsample_image(image):
+    if len(image.shape) == 2:
+        image = quantizeGrayscale(image)
+        image = np.reshape(image, shape=(image.shape[0], image.shape[1], 1))
 
-    h, w = image.shape
+    h, w, c = image.shape
+
+    if h % 2 != 0:
+        h += 1
+        image = np.vstack((image, np.zeros((1, w, c))))
+
+    if w % 2 != 0:
+        w += 1
+        image = np.hstack((image, np.zeros((h, 1, c))))
+
+    sub_img = np.zeros((h//2, w//2, c), dtype=np.float32)
+
+    blk_i = 0
+    for i in range(0, h, 2):
+        blk_j = 0
+        for j in range(0, w, 2):
+            for k in range(c):
+                block = image[i:i+2, j:j+2, k]
+
+                blk_mean = np.round(np.mean(block))
+
+                sub_img[blk_i][blk_j][k] = blk_mean
+
+            blk_j += 1
+
+        blk_i += 1
+
+    return sub_img
+
+def convertGS(image: npt.NDArray, output_file='output.txt'):
+    block_image = subsample_image(image)
+    h, w, _ = block_image.shape
 
     with open(output_file, 'w') as f:
         for i in range(h):
             for j in range(w):
-                f.write(get_level_char(image[i][j]))
+                f.write(get_level_char(block_image[i][j]))
             f.write("\n")
 
         f.close()
 
 def convertRGB(image: npt.NDArray, output_file='output.txt'):
-    print(image.shape)
-    h, w, _ = image.shape
+    block_image = subsample_image(image)
+    #block_image = image
+    h, w, _ = block_image.shape
 
     with open(output_file, 'w') as f:
         for i in range(h):
             for j in range(w):
-                r,g,b = image[i][j]
+                r,g,b = block_image[i][j].astype(np.uint8)
 
-                f.write(f"\x1b[38;2;{r};{g};{b}m{get_level_char(image[i][j], 255)}\x1b[0m")
+                f.write(f"\x1b[38;2;{r};{g};{b}m{get_level_char(block_image[i][j])}\x1b[0m")
             f.write("\n")
 
         f.close()
-
-image = ski.io.imread('./tests/Redwingblackbird1.jpeg')
-grayscale_image = ski.color.rgb2gray(image)
-
-convertRGB(image)
