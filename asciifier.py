8a1b1f58912168db178c694e8828890e033cd5e7
Andrea Lavino <andrealavino1410.2003@gmail.com>
2025-04-21 08:22:36 +0200

    perf: improved performance by using skimage resize method.

--- asciifier.py
+++ asciifier.py
@@ -1,85 +1,45 @@
 import skimage as ski
-import numpy as np
 import numpy.typing as npt
 
-def quantizeGrayscale(image):
-    scaling_factor = 1/256
+LEVELS = """ .'`^",:;Il!i><~+_-?][}{1)(|\\/tfjrxnuvczXYUJCLQ0OZmwqpdbkhao*#MW&8%B@$"""
+NUM_LEVELS = len(LEVELS)
 
-    return np.clip(image / scaling_factor, a_min=0, a_max=255)
+def get_level_char(intensity: float):
+    level_index = round(intensity * NUM_LEVELS) - 1
 
-def is_rgb(image):
-    return len(image.shape) > 2
+    return  LEVELS[level_index]
 
-def get_level_char(intensity, max_value = 255):
-    levels = """$@B%8&WM#*oahkbdpqwmZO0QLCJUYXzcvunxrjft/\\|()1{}[]?-_+~<>i!lI;:,"^`'. """
-    levels = levels[::-1]
+def convertGS(image: npt.NDArray, output_file='output.txt', out_dim=(256,256)):
+    block_image = ski.transform.resize(image, out_dim, anti_aliasing=True)
 
-    if len(intensity) == 1:
-        intensity = intensity[0]
-    else:
-        intensity = 0.2125 * intensity[0] + 0.7154 * intensity[1] + 0.0721 * intensity[2]
+    h, w = block_image.shape
 
-    level_index = round((intensity * len(levels)) / max_value) - 1
+    with open(output_file, 'w') as f:
+        for i in range(h):
+            line = []
 
-    return  levels[level_index]
+            for j in range(w):
+                line.append(get_level_char(block_image[i][j]))
 
-def subsample_image(image):
-    if len(image.shape) == 2:
-        image = quantizeGrayscale(image)
-        image = np.reshape(image, shape=(image.shape[0], image.shape[1], 1))
+            line.append("\n")
+            f.write("".join(line))
 
-    h, w, c = image.shape
+def convertRGB(image: npt.NDArray, output_file='output.txt', out_dim=(256, 256)):
+    block_image = ski.transform.resize(image, out_dim, anti_aliasing=True)
+    block_image = ski.util.img_as_ubyte(block_image)
 
-    if h % 2 != 0:
-        h += 1
-        image = np.vstack((image, np.zeros((1, w, c))))
+    gsBI = ski.color.rgb2gray(block_image)
 
-    if w % 2 != 0:
-        w += 1
-        image = np.hstack((image, np.zeros((h, 1, c))))
-
-    sub_img = np.zeros((h//2, w//2, c), dtype=np.float32)
-
-    blk_i = 0
-    for i in range(0, h, 2):
-        blk_j = 0
-        for j in range(0, w, 2):
-            for k in range(c):
-                block = image[i:i+2, j:j+2, k]
-
-                blk_mean = np.round(np.mean(block))
-
-                sub_img[blk_i][blk_j][k] = blk_mean
-
-            blk_j += 1
-
-        blk_i += 1
-
-    return sub_img
-
-def convertGS(image: npt.NDArray, output_file='output.txt'):
-    block_image = subsample_image(image)
     h, w, _ = block_image.shape
 
     with open(output_file, 'w') as f:
         for i in range(h):
+            line = []
+
             for j in range(w):
-                f.write(get_level_char(block_image[i][j]))
-            f.write("\n")
+                r,g,b = block_image[i][j]
 
-        f.close()
+                line.append(f"\x1b[38;2;{r};{g};{b}m{get_level_char(gsBI[i][j])}\x1b[0m")
 
-def convertRGB(image: npt.NDArray, output_file='output.txt'):
-    block_image = subsample_image(image)
-    #block_image = image
-    h, w, _ = block_image.shape
-
-    with open(output_file, 'w') as f:
-        for i in range(h):
-            for j in range(w):
-                r,g,b = block_image[i][j].astype(np.uint8)
-
-                f.write(f"\x1b[38;2;{r};{g};{b}m{get_level_char(block_image[i][j])}\x1b[0m")
-            f.write("\n")
-
-        f.close()
+            line.append("\n")
+            f.write("".join(line))
